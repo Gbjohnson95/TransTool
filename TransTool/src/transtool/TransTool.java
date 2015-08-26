b595ce6954fcc7d92f1098a2acdab1615922b22a
Grant Johnson <gbjohnson@me.com>
2015-08-26 12:09:23 -0600

    Forcefull Commit is forcefull

--- TransTool/src/transtool/TransTool.java
+++ TransTool/src/transtool/TransTool.java
@@ -1,15 +1,33 @@
 
 package transtool;
 
-//import transtool.xmlTools.*; //Import this when needed
+import javax.xml.stream.XMLStreamException;
+import transtool.xmlTools.*;
 
 public class TransTool {
 
     /**
      * @param args the command line arguments
+     * @throws javax.xml.stream.XMLStreamException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws XMLStreamException {
+        xmlWriter question = new xmlWriter();
+        
+        question.newElement("BaseElement");
+        question.closeElement();
+        
+        question.newElement("BaseElement2", "Some Item");
+        question.closeElement();
+        
+        String test[][] = {{"id","1"},{"grade","95"}};
+        
+        question.newElement("baseElement3", test);
+        question.closeElement();
+        
+        String str = question.toStringAndClose();
+        
+        System.out.print(str);
+        
     }
     
 }
