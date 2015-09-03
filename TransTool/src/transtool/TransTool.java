0b3808ea14c371314126ad7d5980fbfa0e700d69
Grant Johnson <gbjohnson@me.com>
2015-09-03 11:06:34 -0600

    THIS END HERE

--- TransTool/src/transtool/TransTool.java
+++ TransTool/src/transtool/TransTool.java
@@ -28,61 +28,7 @@ public class TransTool {
         XMLParser parse = new XMLParser("./brainhoneymanifest.xml");
     }
 
-    /*
-    public static void main(String[] args) throws XMLStreamException, IOException {
-<<<<<<< HEAD
-        // Test Quiz Maker
-        
 
-        quiz test = new quiz();
-        System.out.print(test.print());
-
-        // Test XML Parser
-        // Test UI, BAD LOOP SOMEWHERE, RUN AT YOUR OWN RISK
-        //Window window = new Window();
-=======
-        xmlWriter question = new xmlWriter();
-
-        question.newElement("BaseElement");
-        question.newElement("People");
-        question.newElement("Jane");
-
-        question.closeElement();
-        question.closeElement();
-
-        question.newElement("Jack", "Some Item");
-        question.closeElement();
-
-        question.newElement("Stan");
-        question.newElementAtribute("Grade", "A+");
-        question.closeElement();
-        question.closeElement();
-
-        String str = question.toStringAndClose();
-
-        System.out.print(str);
-
-        xmlDocument doc = new xmlDocument();
-
-        doc.addElements(str);
-        //doc.filePath();
-        doc.fileName("test.xml");
-        doc.writeToFile();
-        System.out.println("file shoudl have been made");
-
-        
-
-           // public static void main(String[] args) {
-        // TODO code application logic here
-        //XMLParser parse = new XMLParser("./brainhoneymanifest.xml");
-        //}
->>>>>>> origin/master
-    }
-
-    //}
-           }
-       
-     */
 
 
 }
