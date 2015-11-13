c0a2831442894c8b5de5f380164c2a6d37bf8a95
Mark Hall <mhall0717@yahoo.com>
2015-11-13 10:30:31 -0700

    Adding extra text and a long answer question

--- TransTool/src/transtool/quiz/LongAnswerQuestion.java
+++ TransTool/src/transtool/quiz/LongAnswerQuestion.java
@@ -17,6 +17,36 @@ public class LongAnswerQuestion extends BrainhoneyQuestion{
    
     public LongAnswerQuestion(BrainhoneyContents brain, Document document, int id, int item, Element root) {
         super(brain, document, id, item, root);
+                writeHeader();
+
+        Element presentation = (Element) rootItem.getElementsByTagName("presentation").item(0);
+        Element flow = doc.createElement("flow");
+        presentation.appendChild(flow);
+        
+        Element material = doc.createElement("material");
+        Element extension = doc.createElement("response_extension");
+        Element str  = doc.createElement("response_str");
+        
+        flow.appendChild(material);
+        flow.appendChild(extension);
+        flow.appendChild(str);
+        
+        Element matText = doc.createElement("mattext");
+        material.appendChild(matText);
+        
+        matText.setAttribute("texttype","text/html");
+        matText.appendChild(doc.createTextNode(brainhoney.getBody()));
+        
+        Element hasSigned = doc.createElement("questiondb.xml");
+        Element htmlEditor = doc.createElement("d2l_2p0:has_htmleditor");
+        
+        hasSigned.setTextContent("no");
+        htmlEditor.setTextContent("no");
+        
+        extension.appendChild(hasSigned);
+        extension.appendChild(htmlEditor);
+        
+        Element fib = doc.createElement("render_fib");
     }
    
 }
